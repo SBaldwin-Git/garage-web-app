0391f601ecd92d1a2b265e86122b21eb47157406
Sam Baldwin <sambaldwin92@gmail.com>
2023-11-08 16:10:00 +0000

    Implemented fetch vehicles function with placeholder link

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,35 @@
-import './App.css';
+import "./App.css";
+import VehicleList from "../VehicleList/VehicleList";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [vehicles, setVehicles] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  // create use effect to fetch vehicles
+  useEffect(() => {
+    fetchVehicles();
+  }, []);
+
+  async function fetchVehicles() {
+    let response = await fetch("herokuapp link goes here", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let data = await response.json();
+    const newVehicles = data.payload.map((vehicle) => ({
+      vehicleMake: vehicle.make,
+      vehicleModel: vehicle.model,
+      vehicleRegistration: vehicle.registration,
+      vehicleNotes: vehicle.notes,
+    }));
+    // Replace existing vehicles with fetched vehicles
+    setVehicles(newVehicles);
+  }
+
   return (
     <div className="App">
       <h1>TEST</h1>
