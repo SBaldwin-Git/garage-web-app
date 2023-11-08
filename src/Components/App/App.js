a57c434028cce67e956c3ff7aeaa9ebd3259b7f6
Sam Baldwin <sambaldwin92@gmail.com>
2023-11-08 16:27:26 +0000

    Commented functionality that connects with backend. Will try a purely frontend end functioning interface before adapting

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,29 +6,29 @@ function App() {
   const [vehicles, setVehicles] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-  // create use effect to fetch vehicles
-  useEffect(() => {
-    fetchVehicles();
-  }, []);
+  // // create use effect to fetch vehicles
+  // useEffect(() => {
+  //   fetchVehicles();
+  // }, []);
 
-  async function fetchVehicles() {
-    let response = await fetch("herokuapp link goes here", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  // async function fetchVehicles() {
+  //   let response = await fetch("herokuapp link goes here", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
 
-    let data = await response.json();
-    const newVehicles = data.payload.map((vehicle) => ({
-      vehicleMake: vehicle.make,
-      vehicleModel: vehicle.model,
-      vehicleRegistration: vehicle.registration,
-      vehicleNotes: vehicle.notes,
-    }));
-    // Replace existing vehicles with fetched vehicles
-    setVehicles(newVehicles);
-  }
+  //   let data = await response.json();
+  //   const newVehicles = data.payload.map((vehicle) => ({
+  //     vehicleMake: vehicle.make,
+  //     vehicleModel: vehicle.model,
+  //     vehicleRegistration: vehicle.registration,
+  //     vehicleNotes: vehicle.notes,
+  //   }));
+  //   // Replace existing vehicles with fetched vehicles
+  //   setVehicles(newVehicles);
+  // }
 
   return (
     <div className="App">
