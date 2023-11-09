c3a2c48c92618c779791e68a7b760378d136482e
Sam Baldwin <sambaldwin92@gmail.com>
2023-11-09 11:14:30 +0000

    Created demo array

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,33 +2,55 @@ import "./App.css";
 import VehicleList from "../VehicleList/VehicleList";
 import { useState, useEffect } from "react";
 
-function App() {
+function App()
+{
+  class Vehicle {
+    constructor(make, model, registration, notes) {
+      this.make = make;
+      this.model = model;
+      this.registration = registration;
+      this.notes = notes;
+    }
+  }
+
+  const vehicleArray = [
+    new Vehicle("Toyota", "Camry", "ABC123", "Great condition"),
+    new Vehicle("Honda", "Civic", "XYZ789", ""),
+    new Vehicle("Ford", "Fusion", "DEF456", "Minor scratches on the side"),
+    new Vehicle("Chevrolet", "Malibu", "GHI789", ""),
+    new Vehicle("Nissan", "Altima", "JKL012", "Needs oil change"),
+  ];
+  
   const [vehicles, setVehicles] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-  // // create use effect to fetch vehicles
-  // useEffect(() => {
-  //   fetchVehicles();
-  // }, []);
+  {
+    // // create use effect to fetch vehicles
+    // useEffect(() => {
+    //   fetchVehicles();
+    // }, []);
 
-  // async function fetchVehicles() {
-  //   let response = await fetch("herokuapp link goes here", {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
+    // async function fetchVehicles() {
+    //   let response = await fetch("herokuapp link goes here", {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
 
-  //   let data = await response.json();
-  //   const newVehicles = data.payload.map((vehicle) => ({
-  //     vehicleMake: vehicle.make,
-  //     vehicleModel: vehicle.model,
-  //     vehicleRegistration: vehicle.registration,
-  //     vehicleNotes: vehicle.notes,
-  //   }));
-  //   // Replace existing vehicles with fetched vehicles
-  //   setVehicles(newVehicles);
-  // }
+    //   let data = await response.json();
+    //   const newVehicles = data.payload.map((vehicle) => ({
+    //     vehicleMake: vehicle.make,
+    //     vehicleModel: vehicle.model,
+    //     vehicleRegistration: vehicle.registration,
+    //     vehicleNotes: vehicle.notes,
+    //   }));
+    //   // Replace existing vehicles with fetched vehicles
+    //   setVehicles(newVehicles);
+    // }
+  }
+
+  
 
   return (
     <div className="App">
